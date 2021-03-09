81462217513d21dd4c99de24217452082f097cf8
Shetty Ganeshprasad <shettyganeshprasad1998@gmail.com>
2021-03-09 19:34:51 +0530

    Initial SB application

---
+++ src/main/java/com/shetty/Codec/Codecs/CodecsApplication.java
@@ -0,0 +1,13 @@
+package com.shetty.Codec.Codecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shetty/Codec/Codecs/CodecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shetty.Codec.Codecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
